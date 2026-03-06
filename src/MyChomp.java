4d72b1e06fb01377f96bf533847d90695f8357a2
tatsuya-seto <tatsuya_seto28@milton.edu>
2026-03-06 12:06:20 -0500

    class

--- src/MyChomp.java
+++ src/MyChomp.java
@@ -1,101 +1,123 @@
 public class MyChomp {
 
+    int[][] losingBoards = new int[20][3];
+    int[][] winningBoards = new int[20][3];
+
+    int losingCount = 0;
+    int winningCount = 0;
+
     public static void main(String[] args) {
         new MyChomp();
     }
 
-    public MyChomp(){
+    public MyChomp() {
 
-        int i=0;
+        // base losing board
+        addLosingBoard(1,0,0);
 
-        for (int r1 = 3; r1 >= 0; r1--) {
-            for (int r2 = r1; r2 >= 0; r2--) {
-                for (int r3 = r2; r3 >= 0; r3--) {
+        for(int r1=1;r1<=3;r1++){
+            for(int r2=0;r2<=r1;r2++){
+                for(int r3=0;r3<=r2;r3++){
 
-                    if(r1==0){
-                        break;
-                    }
+                    if(r1==1 && r2==0 && r3==0) continue;
 
-                    System.out.println(r1 + "," + r2 + "," + r3);
-                    i++;
-
-                    printOneMoves(r1, r2, r3);
+                    evaluateBoard(r1,r2,r3);
                 }
             }
         }
 
-        System.out.println(i + " boards printed");
+        System.out.println("\nWINNING BOARDS:");
+        printBoards(winningBoards, winningCount);
 
+        System.out.println("\nLOSING BOARDS:");
+        printBoards(losingBoards, losingCount);
     }
 
-    public void printOneMoves(int r1, int r2, int r3) {
+    public void evaluateBoard(int r1,int r2,int r3){
 
-        System.out.println("  ");
-        System.out.println("All possible boards:");
+        boolean winningBoard = false;
 
-        int[] rows = {r1, r2, r3};
+        int[] rows = {r1,r2,r3};
 
-        boolean winningboard = false;
+        for(int row=0;row<3;row++){
 
-        for (int row = 0; row < 3; row++) {
             int length = rows[row];
 
-            for (int col = 1; col <= length; col++) {
+            for(int col=1;col<=length;col++){
 
-                int nr1 = r1;
-                int nr2 = r2;
-                int nr3 = r3;
+                int nr1=r1;
+                int nr2=r2;
+                int nr3=r3;
 
-                int newVal = col - 1;
+                int newVal = col-1;
 
-                if (row == 0) {  //eating in r1
+                if(row==0){
                     nr1 = newVal;
-                    nr2 = Math.min(nr2, newVal);
-                    nr3 = Math.min(nr3, newVal);
+                    nr2 = Math.min(nr2,newVal);
+                    nr3 = Math.min(nr3,newVal);
                 }
-                else if (row == 1) { //eating in r2
+                else if(row==1){
                     nr2 = newVal;
-                    nr3 = Math.min(nr3, newVal);
+                    nr3 = Math.min(nr3,newVal);
                 }
-                else { // eating in r3
+                else{
                     nr3 = newVal;
                 }
 
-                // if this results in a valid non-increasing board
-                if (nr1 >= nr2 && nr2 >= nr3) {
-                    System.out.println(nr1 + "," + nr2 + "," + nr3);
-                    if(nr1==1&&nr2==0&&nr3==0){
-                        winningboard = true;
-                    }
-                    if(nr1==2&&nr2==1&&nr3==0){
-                        winningboard = true;
-                    }
-                    if(nr1==1&&nr2==0&&nr3==0){
-                        winningboard = true;
-                    }
-                    if(nr1==1&&nr2==0&&nr3==0){
-                        winningboard = true;
-                    }
-                    if(nr1==1&&nr2==0&&nr3==0){
-                        winningboard = true;
-                    }
-                    if(nr1==1&&nr2==0&&nr3==0){
-                        winningboard = true;
-                    }
-                    if(nr1==1&&nr2==0&&nr3==0){
-                        winningboard = true;
+                if(nr1==0) continue;
+
+                if(nr1>=nr2 && nr2>=nr3){
+
+                    if(isLosingBoard(nr1,nr2,nr3)){
+                        winningBoard = true;
                     }
                 }
             }
         }
 
-        //winningboard or not
-        if(winningboard==true){
-            System.out.println("WIN BOARD 1,0,0 is possible");
-        } else{
-            System.out.println("LOSE BOARD 1,0,0 is not possible");
+        if(winningBoard){
+            addWinningBoard(r1,r2,r3);
+        }
+        else{
+            addLosingBoard(r1,r2,r3);
+        }
+    }
+
+    public boolean isLosingBoard(int r1,int r2,int r3){
+
+        for(int i=0;i<losingCount;i++){
+            if(losingBoards[i][0]==r1 &&
+                    losingBoards[i][1]==r2 &&
+                    losingBoards[i][2]==r3){
+                return true;
+            }
         }
 
-        System.out.println("_______________________________________");
+        return false;
+    }
+
+    public void addLosingBoard(int r1,int r2,int r3){
+        losingBoards[losingCount][0]=r1;
+        losingBoards[losingCount][1]=r2;
+        losingBoards[losingCount][2]=r3;
+        losingCount++;
+    }
+
+    public void addWinningBoard(int r1,int r2,int r3){
+        winningBoards[winningCount][0]=r1;
+        winningBoards[winningCount][1]=r2;
+        winningBoards[winningCount][2]=r3;
+        winningCount++;
+    }
+
+    public void printBoards(int[][] boards,int count){
+
+        for(int i=0;i<count;i++){
+            System.out.println(
+                    boards[i][0] + "," +
+                            boards[i][1] + "," +
+                            boards[i][2]
+            );
+        }
     }
 }
