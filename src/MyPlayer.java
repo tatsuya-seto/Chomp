051df84968a2c92089c648f449e8aebd526d02ac
tatsuya-seto <tatsuya_seto28@milton.edu>
2026-04-10 12:03:24 -0400

    Comments

--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -32,7 +32,7 @@ public class MyPlayer {
         if (bestMove != null) {
             System.out.println("Choosing: (" + bestMove.x + "," + bestMove.y + ")");
             return bestMove;
-        }
+        }//good
 
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
